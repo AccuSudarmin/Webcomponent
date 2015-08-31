cfc7711dfbf47cb59002b2da2cf595759bba0af0
Azrul <accusudarmin@gmail.com>
2015-08-31 18:41:11 +0800

    Push All

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <script src="./lib/azcomponent/webcomponentsjs/webcomponents.js"></script>
+      <link rel="import" href="./lib/azcomponent/az-ui.html">
+      <link href="" rel="icon" type="image/x-icon" />
+      <title>Webcomponent</title>
+      <style>
+         html, body {
+            margin: 0px;
+            padding: 0px;
+         }
+      </style>
+   </head>
+   <body>
+      <!-- <form action="tes.html" method="get"> -->
+      <div style="margin: 10px">
+         <p> <az-input type="text" placeholder="Username" name="username"> </az-input> </p>
+         <p> <az-button name="save" width="150px" height="30px"> Save </az-button> </p>
+      </div>
+   <!-- </form> -->
+   </body>
+</html>
