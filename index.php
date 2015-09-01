86a387604269cd9216e6197bcfba7da5133f011d
Azrul Sudarmin <azrulsudarmin@gmail.com>
2015-09-01 19:47:46 +0800

    Update slide

--- index.php
+++ index.php
@@ -36,6 +36,16 @@
       <az-slide>
          Slide 2
          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+         <div class="coding">
+            <header> JAVASCRIPT </header>
+            <p>
+               <span class="def-syntax"> Var </span> newName = <span class="string">"test"</span>;
+            </p>
+            <p>
+               <span class="def-syntax"> echo </span> <span class="string">"test"</span>;
+            </p>
+         </div>
       </az-slide>
 
       <az-slide>
@@ -51,19 +61,4 @@
    </az-slides>
    <!-- </form> -->
    </body>
-
-   <script>
-      function fungsi1(callback, data2) {
-         callback(data2);
-      }
-
-      function fungsi2(data) {
-         callback(data2);
-      }
-
-      function fungsi3(data){
-
-      }
-
-   </script>
 </html>
